a533c7627935885b3603e349b4205b85aadf1cb8
kasper <kasper@aidoh.dk>
2013-09-09 18:13:56 +0200

    Now doing backup on each save

--- laeder_remote.php
+++ laeder_remote.php
@@ -7,7 +7,7 @@ $data = json_decode($jsondata, true);
 #echo "\n\t".$data['name']."\n";
 $filename = $data['filename'];
 if ($filename) {
-	$fh = fopen("data/".$filename, 'w') or die("can't open file");
+	$fh = fopen("data/".$filename.".json", 'w') or die("can't open file");
 	fwrite($fh, $jsondata);
 	echo "Oki Doki";
 } else {
@@ -16,9 +16,10 @@ if ($filename) {
 
 #save backup:
 if ($filename) {
-	$fh = fopen("data/".$filename.".bak", 'w') or die("can't open file");
+	$date = new DateTime();
+	$fh = fopen("data/".$filename.($date->format("Y.m.d.His").".json"), 'w') or die("can't open file");
 	fwrite($fh, $jsondata);
-	echo "Oki Doki";
+	echo "Oki Doki 2";
 } else {
 	die("unable to save the game");
 }
